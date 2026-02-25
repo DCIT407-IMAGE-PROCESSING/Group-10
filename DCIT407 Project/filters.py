aefb9bb4b7d9da4e1c5708ed8842746d15e63ed5
Paakwesi Effah Aboagye <142548335+mighty808@users.noreply.github.com>
2026-02-25 10:36:07 +0000

    Delete DCIT407 Project/filters.py

--- DCIT407 Project/filters.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def mean_filter(img, ksize=5):
-    return cv2.blur(img, (ksize, ksize))
-
-def gaussian_filter(img, ksize=5, sigma=0):
-    return cv2.GaussianBlur(img, (ksize, ksize), sigma)
